89d2337400997999c2916ab5e47079951530ce41
Kevin Young <33660908+zeac116@users.noreply.github.com>
2018-03-19 03:56:45 +0000

    Update Flood.py

--- Flood.py
+++ Flood.py
@@ -8,48 +8,25 @@ from Vector import Vector
 
 class Flood:
 
-    def __init__(self, difficulty, player):
+    def __init__(self, player):
 
         self.pos = Vector(250, 1000)
         self.velocity = Vector(0, 0)
 
-        self.image = simplegui.load_image('https://i.imgur.com/8FnmykN.png')
-
-        self.difficulty = difficulty
-
-        if difficulty == 'easy':
-            self.slow = 8
-        elif difficulty == 'medium':
-            self.slow = 5
-        elif difficulty == 'hard':
-            self.slow = 3
+        self.image = simplegui.load_image('https://i.imgur.com/tstIBkD.png')
 
         self.player = player
-        self.counter = 0
-
-    def collide(self):
-        # if interact game over
-        pass
-
-    #if we want flood to move use this method...
-    def update(self, canvas):
-        # while interaction with player == False:
-        canvas.draw_image(self.image, (250, 350), (500, 700), self.pos.getP(), (500, 700))
-        if self.counter % self.slow == 0:
-            self.velocity = Vector(0, -1)
-            self.pos.add(self.velocity)
-        self.counter += 1
 
     #if we don't want flood to move use this method
     def draw(self, canvas):
-        canvas.draw_image(self.image, (250, 350), (500, 700), self.pos.getP(), (500, 700))
+        canvas.draw_image(self.image, (250, 37.5), (500, 75), (250, 662.5), (500, 75))
 
 
 def draw(canvas):
     flood.draw(canvas)
 
 
-flood = Flood('hard', 'p')
+flood = Flood('hard')
 
 frame = simplegui.create_frame("Half-Life 3", 500, 700)
 frame.set_draw_handler(draw)
